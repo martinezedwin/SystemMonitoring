6799c4de04c4908ba100f2a40c0940e437211cdc
Edwin Martinez <edwin.martinez@plus.ai>
2021-02-26 05:21:12 -0800

    added private members to System and defined getter functions for System

--- include/system.h
+++ include/system.h
@@ -9,19 +9,41 @@
 
 class System {
  public:
+  // Declaring Cpu() getter function?
   Processor& Cpu();                   // TODO: See src/system.cpp
+  // Delcaring Processes() getter function
   std::vector<Process>& Processes();  // TODO: See src/system.cpp
+  // Declaring the MemoryUtilization() function
   float MemoryUtilization();          // TODO: See src/system.cpp
+  // Declaring the UpTime() getter function
   long UpTime();                      // TODO: See src/system.cpp
+  // Declaring the TotalProcesses() getter function
   int TotalProcesses();               // TODO: See src/system.cpp
+  // Declaring the RunningProcesses() getter function
   int RunningProcesses();             // TODO: See src/system.cpp
+  // Declaring the Kernel() getter function
   std::string Kernel();               // TODO: See src/system.cpp
+  // Declaring the OperatingSystem() getter function
   std::string OperatingSystem();      // TODO: See src/system.cpp
 
   // TODO: Define any necessary private members
  private:
+  // variable cpu_ of type Processor
   Processor cpu_ = {};
+  // variable processes_ of type vector of Process
   std::vector<Process> processes_ = {};
+  // Created - variable kernel_ of type string
+  std::string kernel_ = {};
+  // Created - variable memory_utilization_ of type float
+  float memory_utilization_ = {};
+  // Created - variable operating_system_ of type string
+  std::string operating_sytem_ = {};
+  // Created - variable running_processes_ of type int
+  int running_processes_ = {};
+  // Created - variable total_processes_ of type int
+  int total_processes_ = {};
+  // Created - variable up_time_ of type long
+  long up_time_ = {};
 };
 
 #endif
--- src/system.cpp
+++ src/system.cpp
@@ -13,26 +13,34 @@ using std::size_t;
 using std::string;
 using std::vector;
 
-// TODO: Return the system's CPU
+// TODO: Return the system's CPU (Done probably?)
+// Defining the System::Cpu() function
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
+// TODO: Return a container composed of the system's processes (Done probably?)
+// Defining the System::Processes() funtion
 vector<Process>& System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+// Returning the kernel_ variable I defined as a private member of System class in system.h
+std::string System::Kernel() { return kernel_; }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+// Returning the memory_utilization_ variable I defined as a private member of System class in system.h
+float System::MemoryUtilization() { return memory_utilization_; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// Returning the operating_sytem_ variable I defined as a private member of System class in system.h
+std::string System::OperatingSystem() { return operating_sytem_; }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+// Returning the running_processes_ variable I defined as a private member of System class in system.h
+int System::RunningProcesses() { return running_processes_; }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+// Returning the total_processes_ variable I defined as a private member of System class in system.h
+int System::TotalProcesses() { return total_processes_; }
 
 // TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// Returning the up_time_ variable I defined as a private member of System class in system.h
+long int System::UpTime() { return up_time_; }
