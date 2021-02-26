4d1b76d58c90630ede01e4aa1b532d35dc67254d
Edwin Martinez <edwin.martinez@plus.ai>
2021-02-26 05:41:25 -0800

    Defined the getter functions, working on overloading <

--- src/process.cpp
+++ src/process.cpp
@@ -11,22 +11,28 @@ using std::to_string;
 using std::vector;
 
 // TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// Returning the pid_ variable created in process.h
+int Process::Pid() { return pid_; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+// Returning the cpu_utilization_ variable created in process.h
+float Process::CpuUtilization() { return cpu_utilization_; }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// Returning the command_ variable created in process.h
+string Process::Command() { return command_; }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// Returning the ram_ variable created in process.h
+string Process::Ram() { return ram_; }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// Returning the user_ variable created in process.h
+string Process::User() { return user_; }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// Returning the up_time_ variable created in process.h
+long int Process::UpTime() { return up_time_; }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
